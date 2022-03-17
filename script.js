696d894b7d74b53855f1c68a175e6565eb0b8d0b
seika333 <fpsdragon333@gmail.com>
2022-03-17 18:59:31 +0700

    add animation

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function getRandom(min, max){
     return Math.floor((Math.random() * max ) + min);
 }
 
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+ }
+ return result;
+}
+
 function getRandomString(){
     let makan = 
     [
@@ -20,6 +30,17 @@ function getRandomString(){
         "Nasi Uduk",
         "Tempat Baru"
     ];
+    
     let makanSize = Object.keys(makan).length;
-    document.getElementById("makan").innerHTML = makan[getRandom(0, makanSize)];
+    let makan2 = makan[getRandom(0, makanSize)];
+    let time = 0;
+    const timer = setInterval(() => {
+      let randomWords = makeid(10);
+      document.getElementById('makan').textContent = randomWords;
+      time += 50;
+      if (time === 2000) {
+        document.getElementById('makan').textContent = makan2;
+        clearInterval(timer);
+      }
+    }, 10);
 }
